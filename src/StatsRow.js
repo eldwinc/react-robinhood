9b15acfd28b82b755811e1499f3549a72be6cc18
eldwinc <eldwincheung@gmail.com>
2021-10-29 16:27:11 -0700

    2:45:37 add chart pics for r panel

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './StatsRow.css';
+import StockSVG from './stock.svg';
 
-function StatsRow() {
+function StatsRow(props) {
   return (
     <div>
       <div className="row">
         <div class="row-intro">
-          {/* <h1>{props?.name}</h1> */}
-          <h1>AAPL</h1>
-          <p>200 shares</p>
-          {/* <p>{props.volume && props.volume + ' shares'}</p> */}
+          <h1>{props.name}</h1>
+          <p>{props.shares && props.shares + ' shares'}</p>
         </div>
-        <div class="row-chart">{/* <img src={StockChart} height={16}/> */}</div>
+        <div class="row-chart">
+          <img src={StockSVG} height={16} />
+        </div>
         <div class="row-numbers">
           <p class="row-price">$200</p>
           {/* <p class="row-price">{props.price}</p> */}
